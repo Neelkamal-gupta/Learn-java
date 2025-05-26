d42b0360f0121af9292e66cdcc9c1c31bb87c6c6
Neelkamal-gupta <167071644+Neelkamal-gupta@users.noreply.github.com>
2025-05-26 23:12:16 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,11 @@
+package Day_01;
+
+// print Hello World! using public class  : 
+
+public class demo {                                // here file name and class name must be same      
+    public static void main(String[] args) {
+        System.out.println("hello Worlds!");
+        
+    }
+    
+}
